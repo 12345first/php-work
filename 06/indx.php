67ff17ea810c32de285cdcce269f496add9af072
Serdyk Evgenii <john4455@mail.ru>
2016-10-15 14:00:17 +0300

    lesson

---
+++ 06/indx.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Calculator</title>
+</head>
+<body>
+<form>
+    <div>
+        <input type="number" name="a" value="0" required>
+    </div>
+    <div>
+        <input type="number" name="b" value="0" required>
+    </div>
+    <div>
+        <select name="operation" style="width:100px">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+    </div>
+    <div>
+        <input type="submit" name="calc" value="Result">
+    </div>
+</form>
+<hr>
+
+<?php
+if($_REQUEST['calc']){
+    $a = $_REQUEST['a'];
+    $b = $_REQUEST['b'];
+    switch ($_REQUEST['operation']) {
+        case '+':
+             echo "$a+$b=",$a+$b;
+            break;
+        case '-':
+            echo "$a-$b=",$a-$b;
+            break;
+        case '*':
+            echo "$a*$b=", $a*$b;
+            break;
+        case '/':
+            echo "$a/$b=", $a/$b;
+            break;
+
+        default:
+            echo "Operation not found!";
+            break;
+    }
+}
+?>
+
+<?php
+echo"<br>";
+var_dump($_REQUEST);
+?>
+</body>
+</html
